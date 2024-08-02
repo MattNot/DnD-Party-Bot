25f2d90d8273e9c8960d2f89ebef2a4d3beba966
Claudiocli <claudiocli.cp@gmail.com>
2024-08-02 16:43:26 +0200

    Revert "Ammend: forgot try statement"
    This reverts commit 13fe132394bb6d11ed9eb9f1c305f5adc3d37980.

--- commands/utility/selectAnnouncementsChannel.js
+++ commands/utility/selectAnnouncementsChannel.js
@@ -26,6 +26,33 @@ export default {
             // }
             interaction.channel.send(`Hey @everyone, è stata programmata una sessione! :eyes:\nTenetevi pronti per ${createdScheduledEvent.scheduledStartsAt}!`);
         });
+        // Update event listenr
+        interaction.client.on(Events.GuildScheduledEventUpdate, async (editedScheduledEventUrl) => {
+            // TODO: too much complexity?
+            // console.log(`Edit on ScheduledEvent - ${editedScheduledEventUrl}`);
+            // interaction.guild.scheduledEvents.fetch(editedScheduledEventUrl).then((scheduledEvent) => {
+            //     interaction.channel.messages.fetch().then((messages) => {
+            //         messages.forEach(message => {
+            //             message.content.includes(scheduledEvent.id);
+            //         });
+            //     });
+            // });
+            interaction.guild.scheduledEvents.fetch(editedScheduledEventUrl).then((oldScheduledEvent, newScheduledEvent) => {
+                interaction.channel.send(`Cambio di programmi @everyone! La sessione ${oldScheduledEvent.name} si terrà alle ore ${new Intl.DateTimeFormat('it-IT', {hour: 'numeric', minute: 'numeric'}).format(newScheduledEvent.scheduledStartAt)} di ${new Intl.DateTimeFormat('it-IT', {weekday: 'long', day: 'numeric', month: 'long'}).format(newScheduledEvent.scheduledStartAt)}`);
+            });
+        });
+        // Delete event listenr
+        interaction.client.on(Events.GuildScheduledEventDelete, async (deleteScheduledEventUrl) => {
+            interaction.guild.scheduledEvents.fetch(deleteScheduledEventUrl).then((scheduledEvent) => {
+                interaction.channel.send(`@everyone, mi dispiace informarvi che la sessione ${scheduledEvent.name} è stata cancellata`);
+            });
+        });
+        // FIXME: Can't understand why it's converting circular structure to BSON
+        // await mongo_client.connect();
+        // let ann = mongo_client.db(process.env.MONGO_DB_NAME).collection(process.env.MONGO_A_COLLECTION_NAME);
+        // await ann.updateOne({guild: interaction.guild}, { $set: {channel: interaction.channel} }, {upsert: true});
+        // mongo_client.close();
+        // Resolve of the interaction
         interaction.reply('Channel succesfully selected');
     }
 };
