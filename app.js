96e94b6acb3431ce0dc4712a0e9a8568d4534797
Claudiocli <claudiocli.cp@gmail.com>
2024-08-02 13:53:14 +0200

    Adjusment on selectAnnouncementsChannel

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { REST, Routes, Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import { commands } from './commands/commands.js';
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 config();
 
@@ -17,11 +17,42 @@ const mongo_client = new MongoClient(uri, {
 });
 console.log('[Mongo Connection] - Connection started');
 
-const client = new Client({intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds], disableEveryone: false});
+const client = new Client({intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildScheduledEvents], disableEveryone: false});
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`[INFO] - Logged in as ${readyClient.user.tag}`);
 });
+client.once(Events.ClientReady, async c => {
+    await mongo_client.connect();
+    let ann = mongo_client.db(process.env.MONGO_DB_NAME).collection(process.env.MONGO_A_COLLECTION_NAME);
+    let cursor = ann.find({});
+    while (await cursor.hasNext()) {
+        cursor.next().then(res => {
+            console.info(`[INFO] - Addind listeners on ${res.guild}`);
+            // Create event listener
+            c.on(Events.GuildScheduledEventCreate, async (createdScheduledEventUrl) => {
+                res.guild.scheduledEvents.fetch(createdScheduledEventUrl).then((scheduledEvent) => {
+                    // Add id to fetch messages? Related to complexity doubts for update
+                    res.channel.send(`Hey @everyone, è stata programmata una sessione! :eyes:\n"${scheduledEvent.name}"\nTenetevi pronti per ${new Intl.DateTimeFormat('it-IT', {weekday: 'long', day: 'numeric', month: 'long'}).format(scheduledEvent.scheduledStartAt)}! La sessione avrà inizio alle ore ${new Intl.DateTimeFormat('it-IT', {hour: 'numeric', minute: 'numeric'}).format(scheduledEvent.scheduledStartAt)}`);
+                });
+            });
+            // Update event listenr
+            c.on(Events.GuildScheduledEventUpdate, async (editedScheduledEventUrl) => {
+                res.guild.scheduledEvents.fetch(editedScheduledEventUrl).then((oldScheduledEvent, newScheduledEvent) => {
+                    res.channel.send(`Cambio di programmi @everyone! La sessione ${oldScheduledEvent.name} si terrà alle ore ${new Intl.DateTimeFormat('it-IT', {hour: 'numeric', minute: 'numeric'}).format(newScheduledEvent.scheduledStartAt)} di ${new Intl.DateTimeFormat('it-IT', {weekday: 'long', day: 'numeric', month: 'long'}).format(newScheduledEvent.scheduledStartAt)}`);
+                });
+            });
+            // Delete event listenr
+            c.on(Events.GuildScheduledEventDelete, async (deleteScheduledEventUrl) => {
+                res.guild.scheduledEvents.fetch(deleteScheduledEventUrl).then((scheduledEvent) => {
+                    res.channel.send(`@everyone, mi dispiace informarvi che la sessione ${scheduledEvent.name} è stata cancellata`);
+                });
+            });
+            console.info(`[INFO] - Added listeners on ${res.guild}`);
+        });
+    }
+    mongo_client.close();
+})
 // Setting up the possible commands of the client
 client.commands = new Collection();
 
@@ -35,7 +66,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
         console.log('[INFO] - Deploying (/) commands');
 
         client.commands.forEach(async element => {
-            const data = await rest.post(
+            await rest.post(
                 Routes.applicationCommands(process.env.APP_ID),
                 {
                     headers: {
