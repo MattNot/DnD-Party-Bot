62499d4789ff2a34c6c3e42b51b3a6dfe56b3b93
Claudiocli <claudiocli.cp@gmail.com>
2025-04-10 14:57:23 +0200

    Adjustment

--- commands/feet_to_meters.py
+++ commands/feet_to_meters.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from math import ceil
 
 class FeetToMeters(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,12 +12,12 @@ class FeetToMeters(commands.Cog):
         description="Convert feet to meters"
     )
     @app_commands.describe(feet="Amount to convert into meters")
-    async def feet_to_meters(self, interaction: discord.Interaction, feet: float):
+    async def feet_to_meters(self, interaction: discord.Interaction, feet: float, approx: bool = True):
         if feet is None:
             await interaction.response.send_message("Invalid number inserted", ephemeral=True)
             return
 
-        result = feet / 3.28084
+        result = ceil(feet / 3.28084) if approx else feet / 3.28084
 
         await interaction.response.send_message(str(result))
 
--- commands/meters_to_feet.py
+++ commands/meters_to_feet.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from math import ceil
 
 class MetersToFeet(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,12 +12,12 @@ class MetersToFeet(commands.Cog):
         description="Convert meters to feet"
     )
     @app_commands.describe(meters="Amount to convert into feet")
-    async def meters_to_feet(self, interaction: discord.Interaction, meters: float):
+    async def meters_to_feet(self, interaction: discord.Interaction, meters: float, approx: bool = True):
         if meters is None:
             await interaction.response.send_message("Invalid number inserted", ephemeral=True)
             return
 
-        result = meters * 3.28084
+        result = ceil(meters * 3.28084) if approx else meters * 3.28084
 
         await interaction.response.send_message(str(result))
 
