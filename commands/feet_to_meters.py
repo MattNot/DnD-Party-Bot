5eaf766711b9ff7b8f8ddce3631ddbf7673c2992
Claudiocli <claudiocli.cp@gmail.com>
2025-04-11 14:39:13 +0200

    Language support on every command
    Only exception is select_announcements_channel that needs more thoughts

--- commands/feet_to_meters.py
+++ commands/feet_to_meters.py
@@ -4,6 +4,15 @@ from discord.ext import commands
 from math import ceil
 from typing import Optional
 
+locales = {
+    "it":   {
+        "invalid_number": "Numero inserito non valido",
+    },
+    "eng":  {
+        "invalid_number": "Invalid number inserted",
+    }
+}
+
 class FeetToMeters(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -15,7 +24,8 @@ class FeetToMeters(commands.Cog):
     @app_commands.describe(feet="Amount to convert into meters")
     async def feet_to_meters(self, interaction: discord.Interaction, feet: float, approx: Optional[bool] = True):
         if feet is None:
-            await interaction.response.send_message("Invalid number inserted", ephemeral=True)
+            locale = interaction.locale if interaction.locale in locales else "eng"
+            await interaction.response.send_message(content=f"{locales[locale]["invalid_number"]}", ephemeral=True)
             return
 
         result = ceil(feet / 3.28084) if approx else feet / 3.28084
