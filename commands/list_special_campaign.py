f7a785f53b9fc3296faaa13967bb2af6298c034e
Claudiocli <claudiocli.cp@gmail.com>
2025-04-21 00:27:43 +0200

    f

--- commands/list_special_campaign.py
+++ commands/list_special_campaign.py
@@ -5,7 +5,7 @@ from pymongo.collection import Collection
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv(dotenv_path='./../.env')
+load_dotenv()
 
 def get_special_campaigns_collection() -> "Collection":
     mongo_uri = f"mongodb+srv://{getenv('MONGO_USER')}:{getenv('MONGO_PASSWD')}@clusterdnd.qxfls1g.mongodb.net/?authSource=admin&retryWrites=true&w=majority&appName=ClusterDnD"
