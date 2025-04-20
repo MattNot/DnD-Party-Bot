5584d83c7906f0eaea95f144561c114240704ff6
Claudiocli <claudiocli.cp@gmail.com>
2025-04-20 18:32:39 +0200

    Temporary fix

--- commands/roll_a_dice.py
+++ commands/roll_a_dice.py
@@ -43,12 +43,12 @@ class RollADice(commands.Cog):
                 chosen = self.roll_expression(roll)
 
             total, breakdown = chosen
-            if results:
-                nc = results[0][1] if results[0] == chosen else results[1][1]
-                breakdown = f"""```diff
-+ {chosen[1]}
-- {nc}
-```"""
+#             if results:
+#                 nc = results[0][1] if results[0] == chosen else results[1][1]
+#                 breakdown = f"""```diff
+# + {chosen[1]}
+# - {nc}
+# ```"""
             response = locales[locale]["result"].format(
                 total=total,
                 breakdown="\n".join(breakdown)
