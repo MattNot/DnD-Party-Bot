025095b0fdf1df9dd6d25bf9a7ffde5883429e16
Claudiocli <claudiocli.cp@gmail.com>
2025-04-22 14:16:52 +0200

    Typo in the db url

--- util/db.py
+++ util/db.py
@@ -8,7 +8,7 @@ class MongoSync:
     def get_client(cls):
         if cls.__client is None:
             cls.__client = MongoClient(
-                f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWD')}@ac-f7n4bxh-shard-00-02.qxfls1g.mongodb.net:27017,ac-f7n4bxh-shard-00-01.qxfls1g.mongodb.net:27017, ac-f7n4bxh-shard-00-00.qxfls1g.mongodb.net:27017/?authSource=admin&retryWrites=true&w=majority&appName=ClusterDnD",
+                f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWD')}@ac-f7n4bxh-shard-00-02.qxfls1g.mongodb.net:27017,ac-f7n4bxh-shard-00-01.qxfls1g.mongodb.net:27017,ac-f7n4bxh-shard-00-00.qxfls1g.mongodb.net:27017/?authSource=admin&retryWrites=true&w=majority&appName=ClusterDnD",
                 serverSelectionTimeoutMS=10000,
                 connectTimeoutMS=10000
             )
