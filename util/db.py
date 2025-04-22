03b3fe5308b5d889cf9dcb22fe375daf13049849
Claudiocli <claudiocli.cp@gmail.com>
2025-04-22 13:09:13 +0200

    Fix ServerSelectionTimeoutError

--- util/db.py
+++ util/db.py
@@ -10,7 +10,8 @@ class MongoSync:
             cls.__client = MongoClient(
                 f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWD')}@clusterdnd.qxfls1g.mongodb.net/?authSource=admin&retryWrites=true&w=majority&appName=ClusterDnD",
                 serverSelectionTimeoutMS=5000,
-                connectTimeoutMS=5000
+                connectTimeoutMS=5000,
+                directConnection=True
             )
         return cls.__client
 
