871a843ed62b6a5c91ec294e0128ed32cde9c8d9
Claudiocli <claudiocli.cp@gmail.com>
2025-05-10 01:28:28 +0200

    Script fix

--- manage_db.py
+++ manage_db.py
@@ -24,7 +24,7 @@ def elaborate(choice: int):
     if choice == 1:
         cursor.execute("SELECT campaign, players, master FROM campaigns")
         rows = cursor.fetchall()
-        print(''.join(r for r in rows))
+        print(f"{campaign}\n{master}\n{players}\n\n" for campaign, players, master in rows)
     if choice == 2:
         campaign = input("Insert Campaign: ").strip()
         user = input("Insert User: ").strip()
